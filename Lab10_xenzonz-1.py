42196bb57345d6e57d04c0edfa961cbf5deb05a1
sam cocquyt <samcocquyt04@gmail.com>
2026-03-29 11:07:36 -0400

    initial code added

--- Lab10_xenzonz-1.py
+++ Lab10_xenzonz-1.py
@@ -1,1 +1,32 @@
 print("hello world")
+
+"""
+Docstring for Lab10_xenzonz_1
+i. LAB 9: Word Count
+ii. Sam Cocquyt
+iii. 
+iv. No starter code
+v. 3/15/2026
+"""
+
+import string
+from pathlib import Path
+
+class WordAnalyzer:
+    
+    def __init__(self, filepath):
+        return 0
+    
+    def process_file(self):
+        #main logic
+        return 0
+    
+    def print_report(self):
+        return 0
+    
+def main():
+    return 0
+
+
+if __name__ == "__main__":
+    main()
