3f1de27d1e7c74f3f46d234fa07616194e6f6ba6
sam cocquyt <samcocquyt04@gmail.com>
2026-03-29 11:11:46 -0400

    docstring

--- Lab10_xenzonz-1.py
+++ Lab10_xenzonz-1.py
@@ -4,7 +4,9 @@ print("hello world")
 Docstring for Lab10_xenzonz_1
 i. LAB 9: Word Count
 ii. Sam Cocquyt
-iii. 
+iii. An OOP-based program that displays a menu of 4 predefined text files, lets the user choose one, then reads and analyzes that file. 
+    The program will count the frequency of every word in the selected file and print an alphabetical report.
+    WordAnalyzer class will accept an optional list of "stop words" during initialization.
 iv. No starter code
 v. 3/15/2026
 """
@@ -26,7 +28,6 @@ class WordAnalyzer:
     
 def main():
     return 0
-
 
 if __name__ == "__main__":
     main()
